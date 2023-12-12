5d0fd87ad24d606e7053c30b22a68d46a96521be
masato0432 <153025768+masato0432@users.noreply.github.com>
2023-12-12 07:09:58 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,30 @@
+# Import the necessary libraries
 import streamlit as st
+import pandas as pd
 import numpy as np
 
-# アプリケーションのタイトル
-st.title('席替えアプリ')
+# Create a function to generate the seat arrangement
+def generate_seat_arrangement():
+    # Create a 7x6 grid of seats
+    seats = np.arange(1, 43).reshape(7, 6)
+    # Randomly shuffle the numbers from 1 to 41
+    np.random.shuffle(seats)
+    return seats
 
-# 開始ボタン
-if st.button('開始'):
-    # 1から41の数字をランダムに並べ替えて、7行6列の席に割り当てる
-    seats = np.random.choice(np.arange(1, 42), size=(7, 6), replace=False)
+# Main function to run the web application
+def main():
+    # Set the title of the web app
+    st.title("席替えの席を決める")
 
-    # 結果の表示
-    st.write(seats)
+    # Add a button to trigger the seat arrangement generation
+    if st.button("開始"):
+        # Call the function to generate the seat arrangement
+        seat_arrangement = generate_seat_arrangement()
+        # Create a pandas DataFrame to display the seat arrangement as a table
+        df = pd.DataFrame(seat_arrangement, columns=[f"Column {i+1}" for i in range(6)], index=[f"Row {i+1}" for i in range(7)])
+        # Display the seat arrangement as a table
+        st.write(df)
+
+# Run the main function
+if __name__ == "__main__":
+    main()
