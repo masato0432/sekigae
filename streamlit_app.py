6aeef5e46e3e6d6afb95526dd2261053d1695f10
masato0432 <153025768+masato0432@users.noreply.github.com>
2023-12-12 07:04:40 +0000

    sekigae

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,15 @@
-# Streamlitライブラリをインポート
+python
 import streamlit as st
+import numpy as np
 
-# ページ設定（タブに表示されるタイトル、表示幅）
-st.set_page_config(page_title="タイトル", layout="wide")
+# アプリケーションのタイトル
+st.title('席替えアプリ')
 
-# タイトルを設定
-st.title('Streamlitのサンプルアプリ')
+# 開始ボタン
+if st.button('開始'):
+    # 7行6列の席を表す配列を作成し、1から41の数字をランダムに割り当てる
+    seats = np.arange(1, 42).reshape(7, 6)
+    np.random.shuffle(seats)
 
-# テキスト入力ボックスを作成し、ユーザーからの入力を受け取る
-user_input = st.text_input('あなたの名前を入力してください')
-
-# ボタンを作成し、クリックされたらメッセージを表示
-if st.button('挨拶する'):
-    if user_input:  # 名前が入力されているかチェック
-        st.success(f'🌟 こんにちは、{user_input}さん! 🌟')  # メッセージをハイライト
-    else:
-        st.error('名前を入力してください。')  # エラーメッセージを表示
-
-# スライダーを作成し、値を選択
-number = st.slider('好きな数字（10進数）を選んでください', 0, 100)
-
-# 補足メッセージ
-st.caption("十字キー（左右）でも調整できます。")
-
-# 選択した数字を表示
-st.write(f'あなたが選んだ数字は「{number}」です。')
-
-# 選択した数値を2進数に変換
-binary_representation = bin(number)[2:]  # 'bin'関数で2進数に変換し、先頭の'0b'を取り除く
-st.info(f'🔢 10進数の「{number}」を2進数で表現すると「{binary_representation}」になります。 🔢')  # 2進数の表示をハイライト
+    # 結果の表示
+    st.write(seats)
